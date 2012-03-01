b73f0958f514d0ad2e28ae34504909185251661b
Luke Atherton <latherton@mcaus.com.au>
2012-03-01 18:55:25 +1000

    Added existing files

---
+++ GUIBluetoothFinder/BluetoothFinderForm.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace GUIBluetoothFinder
+{
+    partial class BluetoothFinderForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.btnFind = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.nodeTree = new System.Windows.Forms.TreeView();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnFind
+            // 
+            this.btnFind.Location = new System.Drawing.Point(6, 6);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(75, 23);
+            this.btnFind.TabIndex = 0;
+            this.btnFind.Text = "Find";
+            this.btnFind.UseVisualStyleBackColor = true;
+            this.btnFind.Click += new System.EventHandler(this.btnFind_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dataGridView1.Location = new System.Drawing.Point(87, 6);
+            this.dataGridView1.Name = "dataGridView1";
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.dataGridView1.Size = new System.Drawing.Size(394, 447);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // nodeTree
+            // 
+            this.nodeTree.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nodeTree.Location = new System.Drawing.Point(87, 6);
+            this.nodeTree.Name = "nodeTree";
+            this.nodeTree.Size = new System.Drawing.Size(394, 447);
+            this.nodeTree.TabIndex = 5;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(6, 6);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(75, 23);
+            this.btnRefresh.TabIndex = 6;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(495, 488);
+            this.tabControl1.TabIndex = 7;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Controls.Add(this.btnFind);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(487, 462);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Search";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.nodeTree);
+            this.tabPage2.Controls.Add(this.btnRefresh);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(487, 462);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Data";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // BluetoothFinderForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(514, 507);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "BluetoothFinderForm";
+            this.Text = "Tooth Finder";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFind;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TreeView nodeTree;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+    }
+}
+
---
+++ GUIBluetoothFinder/BluetoothFinderForm.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using InTheHand.Net; // e.g. BluetoothAddress, BluetoothEndPoint etc
+using InTheHand.Net.Sockets; // e.g. BluetoothDeviceInfo, BluetoothClient, BluetoothListener
+using InTheHand.Net.Bluetooth; // e.g. BluetoothService, BluetoothRadio
+using System.Net;
+using System.Xml;
+using System.Xml.Xsl;
+using System.Xml.Linq;
+
+namespace GUIBluetoothFinder
+{
+    public partial class BluetoothFinderForm : Form
+    {
+        private DataSet ds;
+        private DataTable dt;
+
+        public BluetoothFinderForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ds = new DataSet();
+            dt = new DataTable("Device");
+
+            // Create a new DataTable and set two DataColumn objects as primary keys.
+            DataColumn[] keys = new DataColumn[1];
+            DataColumn column;
+
+            // Create column 1.
+            column = new DataColumn();
+            column.DataType = System.Type.GetType("System.String");
+            column.ColumnName = "Device Name";
+
+            // Add the column to the DataTable.Columns collection.
+            dt.Columns.Add(column);
+
+            // Create column 2 and add it to the array.
+            column = new DataColumn();
+            column.DataType = System.Type.GetType("System.String");
+            column.ColumnName = "MAC Address";
+            dt.Columns.Add(column);
+
+            // Add the column to the array.
+            keys[0] = column;
+
+            // Create column 3.
+            column = new DataColumn();
+            column.DataType = System.Type.GetType("System.String");
+            column.ColumnName = "Device Type";
+
+            // Add the column to the DataTable.Columns collection.
+            dt.Columns.Add(column);
+
+            // Set the PrimaryKeys property to the array.
+            dt.PrimaryKey = keys;
+
+            ds.Tables.Add(dt);
+            dataGridView1.DataSource = ds.Tables[0];
+
+            // Create a new XmlDocument  
+            XmlDocument doc = new XmlDocument();
+
+            // Load data  
+            doc.Load("http://teethtracker.heroku.com/devices.xml");
+
+            // Get nodes
+            XmlNodeList nodes = doc.SelectNodes("/devices/device");
+
+            foreach (XmlNode node in nodes)
+            {
+                dt.Rows.Add(node.SelectSingleNode("device-name").InnerText, node.SelectSingleNode("bluetooth-id").InnerText, "");                               
+            }
+
+            buildTree();
+        }
+
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+            DiscoDevicesAsync();
+            btnFind.Enabled = false;
+        }
+
+        public void DiscoDevicesAsync()
+        {
+            BluetoothComponent bco = new BluetoothComponent();
+            bco.DiscoverDevicesProgress += HandleDiscoDevicesProgress;
+            bco.DiscoverDevicesComplete += HandleDiscoDevicesComplete;
+            bco.DiscoverDevicesAsync(255, true, true, true, false, 99);
+        }
+
+        private void HandleDiscoDevicesProgress(object sender, DiscoverDevicesEventArgs e)
+        {
+            foreach (BluetoothDeviceInfo device in e.Devices)
+            {
+                object[] findTheseVals = new object[1];
+                findTheseVals[0] = device.DeviceAddress.ToString();
+                DataRow foundRow = dt.Rows.Find(findTheseVals);
+                if (foundRow == null)
+                {
+                    dt.Rows.Add(device.DeviceName, device.DeviceAddress.ToString(), device.ClassOfDevice);
+                    WebRequest request = WebRequest.Create("http://teethtracker.heroku.com/devices/new?bluetooth_id=" + device.DeviceAddress.ToString() + "&name=" + device.DeviceName + "&type=" + device.ClassOfDevice.Device.ToString());
+                    request.BeginGetResponse(new AsyncCallback(FinishWebRequest), request);
+                }
+            }
+        }
+
+        private void buildTree()
+        {
+            try
+            {
+                // Initialize the TreeView control.
+                nodeTree.Nodes.Clear();
+                TreeNode tNode = new TreeNode();
+
+                XmlDocument cDom = new XmlDocument();
+                cDom.LoadXml("<nodes></nodes>");
+
+                XDocument devMovements = XDocument.Load("http://teethtracker.heroku.com/device_movements.xml");
+                //XDocument devMovements = XDocument.Load("test.xml");
+
+                var nodeList = from nodeStation in devMovements.Descendants("device-movements").Descendants("device-movement").Descendants("node") select nodeStation.Value;
+                foreach (var nodeName in nodeList.Distinct())
+                {
+                    XmlNode nodeElement = cDom.CreateNode(XmlNodeType.Element, nodeName.ToString(), "");
+
+                    var deviceList = from deviceStation
+                                  in devMovements.Descendants("device-movements").Descendants("device-movement").Elements("device-bluetooth-id")
+                                     select deviceStation.Value;
+
+                    foreach (var deviceName in deviceList.Distinct())
+                    {
+                        var locationChanges = (from c in devMovements.Descendants("device-movements").Descendants("device-movement")
+                                    where String.Compare((string)c.Element("device-bluetooth-id"), deviceName.ToString()) == 0  && (string)c.Element("movement-type") == "arrival"
+                                    orderby (string)c.Element("updated-at")
+                                    select (string)c.Element("node").Value);
+
+                        string currentLocation = locationChanges.Last();
+
+                        if (String.Compare(currentLocation, nodeName.ToString()) == 0)
+                        {
+                            XmlNode deviceElement = cDom.CreateTextNode(deviceName.ToString());
+                            nodeElement.AppendChild(deviceElement);
+                        }
+                    }
+
+                    cDom.DocumentElement.AppendChild(nodeElement);
+                }
+
+                nodeTree.Nodes.Add(new TreeNode(cDom.DocumentElement.Name));
+                tNode = nodeTree.Nodes[0];
+                AddNode(cDom.DocumentElement, tNode);
+
+                // Create a new XmlDocument  
+                XmlDocument deviceDoc = new XmlDocument();
+                XmlDocument deviceTreeDoc = new XmlDocument();
+
+                deviceTreeDoc.LoadXml("<devices></devices>");
+
+                // Load data  
+                deviceDoc.Load("http://teethtracker.heroku.com/devices.xml");
+
+                // Get nodes
+                XmlNodeList deviceNames = deviceDoc.SelectNodes("/devices/device/device-name");
+                foreach (XmlNode name in deviceNames)
+                {
+                    XmlNode newElem = deviceTreeDoc.CreateTextNode(name.InnerText);
+                    newElem.InnerText = name.InnerText;
+                    deviceTreeDoc.DocumentElement.AppendChild(newElem);
+                }
+                
+                nodeTree.Nodes.Add(new TreeNode(deviceTreeDoc.DocumentElement.Name));
+                tNode = nodeTree.Nodes[1];
+                AddNode(deviceTreeDoc.DocumentElement, tNode);
+
+                //nodeTree.ExpandAll();
+            }
+            catch (XmlException xmlEx)
+            {
+                MessageBox.Show(xmlEx.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void AddNode(XmlNode inXmlNode, TreeNode inTreeNode)
+        {
+            XmlNode xNode;
+            TreeNode tNode;
+            XmlNodeList nodeList;
+            int i;
+
+            // Loop through the XML nodes until the leaf is reached.
+            // Add the nodes to the TreeView during the looping process.
+            if (inXmlNode.HasChildNodes)
+            {
+                nodeList = inXmlNode.ChildNodes;
+                for (i = 0; i <= nodeList.Count - 1; i++)
+                {
+                    xNode = inXmlNode.ChildNodes[i];
+                    inTreeNode.Nodes.Add(new TreeNode(xNode.Name));
+                    tNode = inTreeNode.Nodes[i];
+                    AddNode(xNode, tNode);
+                }
+            }
+            else
+            {
+                // Here you need to pull the data from the XmlNode based on the
+                // type of node, whether attribute values are required, and so forth.
+                inTreeNode.Text = (inXmlNode.OuterXml).Trim();
+            }
+        } 
+
+        private void FinishWebRequest(IAsyncResult result)
+        {
+            HttpWebResponse response = (result.AsyncState as HttpWebRequest).EndGetResponse(result) as HttpWebResponse;
+        }
+
+        private void HandleDiscoDevicesComplete(object sender, DiscoverDevicesEventArgs e)
+        {
+            if (e.Cancelled)
+            {
+                Console.WriteLine("DiscoDevicesAsync cancelled.");
+            }
+            else if (e.Error != null)
+            {
+                Console.WriteLine("DiscoDevicesAsync error: {0}.", e.Error.Message);
+            }
+            else
+            {
+                Console.WriteLine("DiscoDevicesAsync complete found {0} devices.", e.Devices.Length);
+            }
+            btnFind.Enabled = true;
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            buildTree();
+        }
+       
+    }
+}
